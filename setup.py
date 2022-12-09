88796562caa564d2db7109f1800800b9891f6f24
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2022-12-09 18:13:51 +0700

    Publish package

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='marian',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='compromise-marian',
     packages=['marian'],
-    version='0.1.0',
-    description='Marian Machine Translation',
+    version='0.1.1',
+    description='Marian model but with two decoders',
     author='Le Minh Khoi',
     license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    py_modules=["compromise-marian"],
+    install_requires=[]
 )
